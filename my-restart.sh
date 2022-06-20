d9e3a4b7e165d2d446a776ac7585464d8e1704b0
adofsauron <adofsauron@163.com>
2022-06-20 18:26:57 +0800

    fix main

--- my-restart.sh
+++ my-restart.sh
@@ -2,5 +2,17 @@
 
 bash my-stop.sh
 
+if [ "0" != "$?" ]; then
+    echo `date` "mysql restart fail, mysql stop fail"
+    exit 1
+fi
+
 bash my-start.sh
 
+if [ "0" != "$?" ]; then
+    echo `date` "mysql restart fail, mysql start fail"
+    exit 2
+fi
+
+echo `date` "mysql restart ok"
+exit 0
