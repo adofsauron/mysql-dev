886ff04fae244233f75066f40f2195c4e1dc8a1e
adofsauron <adofsauron@163.com>
2022-06-21 11:04:07 +0800

    fix main

--- install-base.sh
+++ install-base.sh
@@ -4,5 +4,5 @@ yum install -y gcc gcc-c++ \
     gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ gcc-toolset-10-binutils \
     python3 python3-devel libicu libicu-devel zlib zlib-devel bzip2 bzip2-devel \
     make cmake automake \
-    pigz 
+    pigz zstd libzstd-devel 
 
