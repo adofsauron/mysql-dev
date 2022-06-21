de31d6264efa4cd53bc740875e30685c9ceb4a7c
adofsauron <adofsauron@163.com>
2022-06-21 10:26:44 +0800

    fix main

--- build-3rd.sh
+++ build-3rd.sh
@@ -5,14 +5,17 @@ HERE=`pwd`
 
 cd 3rd
 
-rm boost_1_73_0 -rf
+# rm boost_1_73_0 -rf
 
-echo `date` "tar -xvjf boost_1_73_0.tar.bz2"
-tar -xvjf boost_1_73_0.tar.bz2
+if [ ! -d boost_1_73_0 ]; then
 
-if [ "0" != "$?" ]; then
-    echo `date` "tar -xvjf boost_1_73_0.tar.bz2 fail"
-    exit 1
+    echo `date` "tar -xvjf boost_1_73_0.tar.bz2"
+    tar -xvjf boost_1_73_0.tar.bz2
+
+    if [ "0" != "$?" ]; then
+        echo `date` "tar -xvjf boost_1_73_0.tar.bz2 fail"
+        exit 1
+    fi
 fi
 
 cd boost_1_73_0
