bfeb449f52fcdbf05a2ee3a16f70753eee304f1a
adofsauron <adofsauron@163.com>
2022-06-21 10:14:42 +0800

    fix main

---
+++ rebuild.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+MYSQL_PATH=/usr/local/mysql
+
+HERE=`pwd`
+
+cd trunk
+
+cd mysql-server-mysql-8.0.28
+
+rm -rf build 
+
+if [ ! -d build ]; then
+    mkdir -p build
+    cd build
+    # verify prefix path
+    cmake .. -DCMAKE_INSTALL_PREFIX=$MYSQL_PATH
+else
+    cd build
+fi
+
+# build
+make -j"$(nproc)"
+
+if [ "0" != "$?" ]; then
+    echo `date` "build fail"
+    exit 1
+fi
+
+make install
+
+if [ "0" != "$?" ]; then
+    echo `date` "make install fail"
+    exit 1
+fi
+
+if [ "0" == "`grep "$MYSQL_PATH/bin"  -rn /etc/profile  | wc -l`" ]; then
+    echo -e '\n\nexport PATH='$MYSQL_PATH'/bin:$PATH\n' >> /etc/profile && source /etc/profile
+fi
+
+if [ "1" != "`which mysqld | wc -l`" ]; then
+    echo `date` "export mysql/bin fail"
+    exit 1
+fi
+
+## systemctl 
+
+# if [ -f "/etc/init.d/mysqld" ]; then
+#     rm -rf /etc/init.d/mysqld.back
+#     mv /etc/init.d/mysqld /etc/init.d/mysqld.back
+# fi
+
+# cp $MYSQL_PATH/support-files/mysql.server /etc/init.d/mysqld
+# chmod +x /etc/init.d/mysqld
+# systemctl enable mysqld
+
+echo "over"
+cd $HERE
+
+exit 0
+
