f52e9fd58a52ac7d5543575b644914e83182896b
adofsauron <adofsauron@163.com>
2022-06-24 23:19:00 +0800

    fix main

--- install-base.sh
+++ install-base.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+dnf install https://dl.Fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf update -y
+
 yum install -y gcc gcc-c++ \
     gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ gcc-toolset-10-binutils \
     python3 python3-devel libicu libicu-devel zlib zlib-devel bzip2 bzip2-devel \
