9a99ef5476fe9dc972d235d06103fa05cb72556b
adofsauron <adofsauron@163.com>
2022-06-25 14:53:36 +0800

    fix main

--- build-with-3rd.bat
+++ build-with-3rd.bat
@@ -1,6 +1,6 @@
 
 cd trunk/mysql-server-mysql-8.0.28/build-x86
 
-cmake .. -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost-x86
+"D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" .. -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost-x86
 
 cd ../../..
