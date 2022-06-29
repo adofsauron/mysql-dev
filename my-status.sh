31bed6453fe3caa0192ad0f85323e12cf852d2b4
adofsauron <adofsauron@163.com>
2022-06-29 20:45:37 +0800

    fix main

--- my-status.sh
+++ my-status.sh
@@ -2,7 +2,7 @@
 
 MYSQL_PATH=/usr/local/mysql
 
-PID=`ps -aux | grep $MYSQL_PATH/bin/mysqld | grep -v grep | awk -F ' ' '{print $2}'`
+PID=`ps -aux | grep $MYSQL_PATH/bin/mysqld | grep -v safe | grep -v grep | awk -F ' ' '{print $2}'`
 
 echo "PID=$PID"
 
