2dd5875b37f9d8ba178719b863efd8ec2803d229
kevin <adofsauron@163.com>
2022-06-30 22:13:17 +0800

    fix main

--- install-base.sh
+++ install-base.sh
@@ -7,5 +7,6 @@ yum install -y gcc gcc-c++ \
     gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ gcc-toolset-10-binutils \
     python3 python3-devel libicu libicu-devel zlib zlib-devel bzip2 bzip2-devel \
     make cmake automake \
-    pigz zstd libzstd-devel openssl-devel 
+    pigz zstd libzstd-devel \
+    openssl-devel ncurses-devel
 
